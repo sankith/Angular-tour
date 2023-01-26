0fbeead72e6bc9dc30636ff59430b96d72d4b6bb
sankith <thurpusankith56@gmail.com>
2023-01-26 16:42:42 +0530

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { BaselayoutComponent } from './baselayout/baselayout.component'
+import { HomeComponent } from './home/home.component';
+import { PackagesComponent } from "./packages/packages.component";
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'packages', component: PackagesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', pathMatch: 'full', component: AboutComponent},  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HomeComponent } from './home/home.component';
+import { PackagesComponent } from './packages/packages.component';
+import { AboutComponent } from './about/about.component';
+import { BaselayoutComponent } from './baselayout/baselayout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PackagesComponent,
+    AboutComponent,
+    BaselayoutComponent,
   ],
   imports: [
     BrowserModule,
