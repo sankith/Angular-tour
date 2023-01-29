906f0db916a3f13b0754e415b1f59223c9add0d4
sankith <thurpusankith56@gmail.com>
2023-01-29 15:13:21 +0530

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HostListener} from '@angular/core';
+import { faFacebookSquare, faInstagram, faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import {HostListener} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faFacebookSquare = faFacebookSquare;
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
 
   constructor() { }
 
